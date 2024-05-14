f9ad62663d5fe2c7cec95fe76509da93309db8a8
xgan <xgan2018@163.com>
2024-05-14 16:16:54 +0800

    set up git gan

---
+++ components/LessonCompleteButton.client.vue
@@ -0,0 +1,28 @@
+<template>
+  <label :class="modelValue ? 'text-green-500' : 'text-red-500'">
+    <input
+      type="checkbox"
+      :value="modelValue"
+      @input="() => $emit('update:modelValue', !modelValue)"
+      class="hidden"
+    />
+    {{ modelValue ? "Completed" : "Not Completed" }}
+  </label>
+</template>
+
+<script setup>
+defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+defineEmits(["update:modelValue"]);
+</script>
+
+<style scoped>
+::selection {
+  display: none;
+}
+</style>
---
+++ components/VideoPlayer.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const props = defineProps({
+  videoId: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    title="Video player"
+    frameborder="0"
+    allow="autoplay; fullscreen; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</template>
+
+<style scoped></style>
